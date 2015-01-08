04f43266425b5877b2d92c7295e1552879fc26a4
Jerry <quinteroheg2@lopers.unk.edu>
2015-01-08 14:41:26 -0600

    Started, but do not have the file to finish.

---
+++ Challenges/src/Chapter13/ShoppingCartSystem.java
@@ -0,0 +1,19 @@
+package Chapter13;
+
+import javax.swing.*;
+
+/**
+ * Created by Gerardo on 1/6/2015.
+ */
+public class ShoppingCartSystem extends JFrame {
+
+    public ShoppingCartSystem() {
+        setTitle("Shopping Cart System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
