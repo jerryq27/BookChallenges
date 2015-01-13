5b132793e4e050fb1705f7a086ef92d61475a16c
Jerry <quinteroheg2@lopers.unk.edu>
2015-01-12 19:56:25 -0600

    Finished the CellPhonePackages GUI.

--- Challenges/src/Chapter13/CellPhonePackages.java
+++ Challenges/src/Chapter13/CellPhonePackages.java
@@ -2,6 +2,10 @@ package Chapter13;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.Format;
 
 /**
  * Created by Gerardo on 1/7/2015.
@@ -16,6 +20,8 @@ public class CellPhonePackages extends JFrame {
 
     private JLabel packageLabel, phoneLabel, otherLabel, totalLabel;
     private JTextField packageField, phoneField, otherField, totalField;
+    private double total = 0;
+    private DecimalFormat formatter = new DecimalFormat("##.00");
 
     public CellPhonePackages() {
         setTitle("Cell Phone Packages");
@@ -80,6 +86,31 @@ public class CellPhonePackages extends JFrame {
         package2 = new JMenuItem("800 minutes/month");
         package3 = new JMenuItem("1500 minutes/month");
 
+        package1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                packageField.setText("$45.00");
+                total += 45;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+        package2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                packageField.setText("$65.00");
+                total += 65;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+        package3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                packageField.setText("$99.00");
+                total += 99;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+
         packages.add(package1);
         packages.add(package2);
         packages.add(package3);
@@ -92,6 +123,32 @@ public class CellPhonePackages extends JFrame {
         phone2 = new JMenuItem("Model 110");
         phone3 = new JMenuItem("Model 200");
 
+
+        phone1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                phoneField.setText("$29.95");
+                total += 29.95;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+        phone2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                phoneField.setText("$49.95");
+                total += 49.95;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+        phone3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                phoneField.setText("$99.95");
+                total += 99.95;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+
         phones.add(phone1);
         phones.add(phone2);
         phones.add(phone3);
@@ -102,6 +159,23 @@ public class CellPhonePackages extends JFrame {
 
         other1 = new JMenuItem("Voice mail");
         other2 = new JMenuItem("Text messaging");
+
+        other1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                otherField.setText("$5.00");
+                total += 5;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
+        other2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                otherField.setText("$10.00");
+                total += 10;
+                totalField.setText("$" + formatter.format(total));
+            }
+        });
 
         other.add(other1);
         other.add(other2);
